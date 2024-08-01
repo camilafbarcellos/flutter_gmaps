2c0f012dfda2950a729a7ec50057df2e30faedbb
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-07-31 22:48:22 -0300

    feat: add register screen

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,219 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gmaps/screens/home_screen.dart';
+import '/components/my_buttom.dart';
+import '/components/my_textfield.dart';
+import '/components/square_tile.dart';
+import '../services/auth_service.dart';
+
+class RegisterScreen extends StatefulWidget {
+  final Function()? onTap;
+  RegisterScreen({super.key, required this.onTap});
+
+  @override
+  State<RegisterScreen> createState() {
+    return RegisterScreenState();
+  }
+}
+
+class RegisterScreenState extends State<RegisterScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  void _handleSignUp() async {
+    // show loading circle
+    showDialog(
+        context: context,
+        builder: (context) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        });
+
+    try {
+      // check if both password and confirm password is same
+      if (_passwordController.text == _confirmPasswordController.text) {
+        UserCredential userCredential =
+            await _auth.createUserWithEmailAndPassword(
+          email: _emailController.text,
+          password: _passwordController.text,
+        );
+        print('Usuário ${userCredential.user!.email} registrado!');
+        // pop the loading circle
+        Navigator.pop(context);
+        // go to home screen
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      } else {
+        // pop the loading circle
+        Navigator.pop(context);
+        // show error password don't match
+        genericErrorMessage("As senhas não correspondem!");
+      }
+    } on FirebaseAuthException catch (e) {
+      // pop the loading circle
+      Navigator.pop(context);
+      // show errors messsages
+      if (e.code == 'weak-password') {
+        genericErrorMessage('A senha é muito fraca, tente novamente!');
+      } else if (e.code == 'email-already-in-use') {
+        genericErrorMessage('Email já cadastrado, efetue o login!');
+      } else {
+        // another error
+        genericErrorMessage(e.code);
+      }
+    }
+  }
+
+  void genericErrorMessage(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Erro'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 243, 243, 243),
+      resizeToAvoidBottomInset: true,
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                const Icon(
+                  Icons.lock,
+                  size: 100,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Por favor, preenchar os seus dados para continuar. ',
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 15,
+                  ),
+                ),
+                const SizedBox(height: 25),
+
+                MyTextField(
+                  controller: _emailController,
+                  hintText: 'Email',
+                  icon: Icon(Icons.email_outlined),
+                  obscureText: false,
+                ),
+                const SizedBox(height: 15),
+
+                MyTextField(
+                  controller: _passwordController,
+                  hintText: 'Senha',
+                  icon: Icon(Icons.lock_outline),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 15),
+
+                MyTextField(
+                  controller: _confirmPasswordController,
+                  hintText: 'Confirmar senha',
+                  icon: Icon(Icons.shield_outlined),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 15),
+
+                MyButton(
+                  onTap: _handleSignUp,
+                  formKey: _formKey,
+                  text: 'Registrar-se',
+                ),
+                const SizedBox(height: 20),
+
+                // continue with
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey.shade400,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8, right: 8),
+                        child: Text(
+                          'OR',
+                          style: TextStyle(color: Colors.grey.shade600),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey.shade400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                //google button
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SquareTile(
+                      onTap: () => AuthService().signInWithGoogle(),
+                      imagePath: 'lib/icons/google.svg',
+                      height: 70,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+
+                // already a member? login now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Já possui uma conta? ',
+                      style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                    ),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: Text(
+                        'Faça login',
+                        style: TextStyle(
+                            color: Colors.blue[900],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
