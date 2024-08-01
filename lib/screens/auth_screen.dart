29a6af634fb2731ce134c3df68f7bf237d685231
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-08-01 01:27:27 -0300

    refactor: turn auth screen into stateful widget

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,20 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'home_screen.dart';
+import 'package:flutter_gmaps/screens/markers_list_screen.dart';
 import 'login_or_register_toggle.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() {
+    return AuthScreenState();
+  }
+}
+
+class AuthScreenState extends State<AuthScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
+        stream: _auth.authStateChanges(),
         builder: (context, snapshot) {
           // user logged in
           if (snapshot.hasData) {
-            return HomeScreen();
+            return MarkersListScreen();
           }
           //user not logged in
           else {
