f1a558ccc821458d4aeaecd65306ab0a41d1fa09
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-08-14 15:55:52 -0300

    refactor: refactor google auth service

--- lib/services/google_auth_service.dart
+++ lib/services/google_auth_service.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class AuthService {
+class GoogleAuthService {
   final GoogleSignIn googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
