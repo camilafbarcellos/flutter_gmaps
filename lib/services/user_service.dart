708e740781dde072fc28669192319b4e2075454d
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-08-15 00:41:03 -0300

    feat: add user service

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,12 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import '/models/user.dart';
 
 class UserService {
   final _currentUser = FirebaseAuth.instance.currentUser!;
   final CollectionReference _users =
       FirebaseFirestore.instance.collection('users');
 
-  getUser() async {
-    return await _users.doc(_currentUser.uid).get();
+  Future<UserModel?> getUser() async {
+    DocumentSnapshot userDoc = await _users.doc(_currentUser.uid).get();
+
+    // return UserModel.fromDocument(userDoc);
+    return UserModel.fromSnap(userDoc);
   }
 }
